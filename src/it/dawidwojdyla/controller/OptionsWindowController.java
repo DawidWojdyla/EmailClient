53ee1b96976fde0dee71c427fb4fa3ffdc0e422c
Dawid <dawidw23@poczta.onet.pl>
2020-11-27 00:11:41 +0100

    Implements Initializable interface into OptionsWindowController and set up themePicker and fontSizePicker after the controller creating

--- src/it/dawidwojdyla/controller/OptionsWindowController.java
+++ src/it/dawidwojdyla/controller/OptionsWindowController.java
@@ -1,22 +1,31 @@
 package it.dawidwojdyla.controller;
 
 import it.dawidwojdyla.EmailManager;
+import it.dawidwojdyla.view.ColorTheme;
+import it.dawidwojdyla.view.FontSize;
 import it.dawidwojdyla.view.ViewFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Dawid on 2020-11-26.
  */
-public class OptionsWindowController extends AbstractController {
+public class OptionsWindowController extends AbstractController implements Initializable {
 
     @FXML
     private Slider fontSizePicker;
 
     @FXML
-    private ChoiceBox<?> themePicker;
+    private ChoiceBox<ColorTheme> themePicker;
 
     public OptionsWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -35,5 +44,43 @@ public class OptionsWindowController extends AbstractController {
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setUpThemePicker();
+        setUpFontSizePicker();
 
+    }
+
+    private void setUpFontSizePicker() {
+        fontSizePicker.setMin(0);
+        fontSizePicker.setMax(FontSize.values().length-1);
+        fontSizePicker.setValue(viewFactory.getFontSize().ordinal());
+        fontSizePicker.setMinorTickCount(0);
+        fontSizePicker.setMajorTickUnit(1);
+        fontSizePicker.setBlockIncrement(1);
+        fontSizePicker.setSnapToTicks(true);
+        fontSizePicker.setShowTickMarks(true);
+        fontSizePicker.setShowTickLabels(true);
+        fontSizePicker.setLabelFormatter(new StringConverter<Double>() {
+            @Override
+            public String toString(Double aDouble) {
+                int i = aDouble.intValue();
+                return FontSize.values()[i].toString();
+            }
+
+            @Override
+            public Double fromString(String s) {
+                return null;
+            }
+        });
+        fontSizePicker.valueProperty().addListener((obs, oldVal, newVal) -> {
+            fontSizePicker.setValue(newVal.intValue());
+        });
+
+    }
+
+    private void setUpThemePicker() {
+        themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values()));
+        themePicker.setValue(viewFactory.getColorTheme());
+    }
 }
