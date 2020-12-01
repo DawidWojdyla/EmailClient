1b0c82885746603e0e157a995d4c8d6830e2363e
Dawid <dawidw23@poczta.onet.pl>
2020-12-01 21:48:33 +0100

    Written EmailAccount class including gmail.com configuration properties

---
+++ src/it/dawidwojdyla/model/EmailAccount.java
@@ -0,0 +1,53 @@
+package it.dawidwojdyla.model;
+
+import javax.mail.Store;
+import java.util.Properties;
+
+/**
+ * Created by Dawid on 2020-12-01.
+ */
+public class EmailAccount {
+
+    private String address;
+    private String password;
+    private Properties properties;
+    private Store store;
+
+    public EmailAccount(String address, String password) {
+        this.address = address;
+        this.password = password;
+
+        properties = new Properties();
+        properties.put("incomingHost", "imap.gmail.com");
+        properties.put("mail.store.protocol", "imap");
+        properties.put("mail.transport.protocol", "smtps");
+        properties.put("mail.smtps.host", "smtp.gmail.com");
+        properties.put("mail.smtps.auth", "true");
+        properties.put("outgoinghost", "smtp.gmsil.com");
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+}
