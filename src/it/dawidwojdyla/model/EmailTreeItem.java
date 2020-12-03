a704e501f3cb44afcb0aee7ad95b5ddfb8282ec4
Dawid <dawidw23@poczta.onet.pl>
2020-12-03 02:44:36 +0100

    Written addEmail() method which construct EmailMessage object from JavaMail.Message object

--- src/it/dawidwojdyla/model/EmailTreeItem.java
+++ src/it/dawidwojdyla/model/EmailTreeItem.java
@@ -1,6 +1,13 @@
 package it.dawidwojdyla.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
+
+import javax.mail.Flags;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by Dawid on 2020-12-02.
@@ -8,9 +15,26 @@ import javafx.scene.control.TreeItem;
 public class EmailTreeItem<String> extends TreeItem<String> {
 
     private String name;
+    private ObservableList<EmailMessage> emailMessages;
 
     public EmailTreeItem(String name) {
         super(name);
         this.name = name;
+        this.emailMessages = FXCollections.observableArrayList();
+    }
+
+    public void addEmail(Message message) throws MessagingException {
+        boolean messageIsRead = message.getFlags().contains(Flags.Flag.SEEN);
+        EmailMessage emailMessage = new EmailMessage(
+                message.getSubject(),
+                message.getFrom()[0].toString(),
+                message.getRecipients(MimeMessage.RecipientType.TO)[0].toString(),
+                message.getSize(),
+                message.getSentDate(),
+                messageIsRead,
+                message );
+        emailMessages.add(emailMessage);
+        //to check if it works
+        System.out.println("added to " + name + " " + message.getSubject());
     }
 }
