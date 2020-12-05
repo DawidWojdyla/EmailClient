ca5c43ac9e0f040598afb17c3aacc8f66aa4fe98
Dawid <dawidw23@poczta.onet.pl>
2020-12-05 15:26:27 +0100

    Created skeleton of EmailSenderService class

---
+++ src/it/dawidwojdyla/controller/services/EmailSenderService.java
@@ -0,0 +1,34 @@
+package it.dawidwojdyla.controller.services;
+
+import it.dawidwojdyla.model.EmailAccount;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+/**
+ * Created by Dawid on 2020-12-05.
+ */
+public class EmailSenderService extends Service {
+
+    private EmailAccount emailAccount;
+    private String subject;
+    private String recipient;
+    private String messageContent;
+
+
+    public EmailSenderService(EmailAccount emailAccount, String subject, String recipient, String messageContent) {
+        this.emailAccount = emailAccount;
+        this.subject = subject;
+        this.recipient = recipient;
+        this.messageContent = messageContent;
+    }
+
+    @Override
+    protected Task createTask() {
+        return new Task() {
+            @Override
+            protected Object call() throws Exception {
+                return null;
+            }
+        };
+    }
+}
