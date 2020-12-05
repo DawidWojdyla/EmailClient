4e285151642fb87cd48885cc0ca09487f07f91ee
Dawid <dawidw23@poczta.onet.pl>
2020-12-05 01:38:22 +0100

    Created skeleton of controller for ComposeMessageWindow

---
+++ src/it/dawidwojdyla/controller/ComposeMessageWindowController.java
@@ -0,0 +1,45 @@
+package it.dawidwojdyla.controller;
+
+import it.dawidwojdyla.EmailManager;
+import it.dawidwojdyla.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Dawid on 2020-12-05.
+ */
+public class ComposeMessageWindowController extends AbstractController implements Initializable {
+
+    @FXML
+    private TextField recipientTextField;
+
+    @FXML
+    private TextField subjectTextField;
+
+    @FXML
+    private HTMLEditor htmlEditor;
+
+    @FXML
+    private Label errorLabel;
+
+    public ComposeMessageWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
+    @FXML
+    void sendButtonAction() {
+
+        System.out.println("Send button in the action!");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
