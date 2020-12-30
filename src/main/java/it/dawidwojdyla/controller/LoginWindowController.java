8b85de62dc9604ba83d3be6c6ae3ee488f5bd553
Dawid <dawidw23@poczta.onet.pl>
2020-12-30 20:52:18 +0100

    LoginWindowController - implemented changes of LoginWindow.fxml from last commit

--- src/main/java/it/dawidwojdyla/controller/LoginWindowController.java
+++ src/main/java/it/dawidwojdyla/controller/LoginWindowController.java
@@ -1,17 +1,22 @@
 package it.dawidwojdyla.controller;
 
 import it.dawidwojdyla.EmailManager;
+import it.dawidwojdyla.controller.oauth.Oauth;
 import it.dawidwojdyla.controller.services.LoginService;
 import it.dawidwojdyla.model.EmailAccount;
 import it.dawidwojdyla.view.ViewFactory;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
@@ -28,6 +33,19 @@ public class LoginWindowController extends AbstractController implements Initial
     @FXML
     private Label errorLabel;
 
+    @FXML
+    private TextField incomingHostField;
+
+    @FXML
+    private TextField outgoingHostField;
+
+    @FXML
+    private CheckBox oauthCheckBox;
+
+    @FXML
+    private CheckBox manualHostCheckBox;
+
+
     public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
     }
@@ -36,7 +54,19 @@ public class LoginWindowController extends AbstractController implements Initial
     void loginButtonActon() {
 
         if(fieldsAreValid()) {
-            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+
+            Properties properties = new Properties();
+            properties.put("incomingHost", incomingHostField.getText());
+            properties.put("outgoingHost", outgoingHostField.getText());
+
+            if (oauthCheckBox.isSelected()) {
+                properties.putAll(emailManager.getOauthDefaultMailProperties());
+                //manage with oauth authorization
+            } else {
+                properties.putAll(emailManager.getDefaultMailProperties());
+            }
+
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText(), properties);
             LoginService loginService = new LoginService(emailAccount, emailManager);
             loginService.start();
             loginService.setOnSucceeded(event -> {
@@ -63,8 +93,8 @@ public class LoginWindowController extends AbstractController implements Initial
 
     private boolean fieldsAreValid() {
 
-        if(emailAddressField.getText().isEmpty()) {
-            errorLabel.setText("Please fill email");
+        if(emailAddressField.getText().isEmpty() || !emailAddressField.getText().contains("@")) {
+            errorLabel.setText("Please fill correct email");
             return false;
         }
 
@@ -79,7 +109,35 @@ public class LoginWindowController extends AbstractController implements Initial
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //just for now (3lines)
         emailAddressField.setText("dawidmailtest@gmail.com");
         passwordField.setText("Dawidmailtest123haslo");
+        setStandardHosts(emailAddressField.getText());
+
+        disableHostFields(true);
+        manualHostCheckBox.setOnAction(e -> {
+            boolean disableHostFieldCondition = !manualHostCheckBox.isSelected();
+            disableHostFields(disableHostFieldCondition);
+            if (disableHostFieldCondition) {
+                setStandardHosts(emailAddressField.getText());
+            }
+        });
+
+        emailAddressField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (!manualHostCheckBox.isSelected()) {
+                setStandardHosts(newValue);
+            }
+        });
+    }
+
+    private void setStandardHosts(String newValue) {
+        String domain = newValue.substring(newValue.indexOf("@") + 1);
+        incomingHostField.setText("imap." + domain);
+        outgoingHostField.setText("smtp." + domain);
+    }
+
+    private void disableHostFields(boolean disableCondition) {
+        incomingHostField.setDisable(disableCondition);
+        outgoingHostField.setDisable(disableCondition);
     }
 }
